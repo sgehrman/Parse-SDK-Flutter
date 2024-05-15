10eea5b3a83c665eba00af0d23d5aaee1f5edda1
Steve Gehrman <sgehrman@gmail.com>
2024-05-14 19:27:30 -0700

    sdf

--- packages/dart/lib/src/network/parse_client.dart
+++ packages/dart/lib/src/network/parse_client.dart
@@ -1,4 +1,4 @@
-part of flutter_parse_sdk;
+part of '../../../parse_server_sdk.dart';
 
 typedef ParseClientCreator = ParseClient Function(
     {required bool sendSessionId, SecurityContext? securityContext});
